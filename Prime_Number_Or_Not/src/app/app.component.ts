e8398b5d4586cf5825e59281bac25eb281bae62d
snehal renushe <snehalrenushe15@gmail.com>
2022-08-10 22:24:12 +0530

    To check number is prime or not

---
+++ Prime_Number_Or_Not/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent
+{
+  public value:any;
+
+  public i: any = 2;
+
+  // public Send(data : any)
+  // {
+  //   this.value = data;
+  // }
+
+  public Check(data : any)
+  {
+    for(data;data>this.i;this.i++)
+    {
+        if(data % this.i==0)
+        {
+            break;
+        }
+    }
+    if(data==this.i)
+    {
+        console.log("prime no");
+    }
+    else
+    {
+      console.log("not prime no");
+    }
+  }
+
+}
